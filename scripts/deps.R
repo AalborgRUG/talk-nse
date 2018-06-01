3044ec048aad41b7fe3724a5e99d013c434878d4
Janus Valberg-Madsen <janus@math.aau.dk>
2018-06-01 14:55:57 +0200

    Start making presentation with xaringan

---
+++ scripts/deps.R
@@ -0,0 +1,20 @@
+# Dependency script, ensures all dependencies are met prior to
+# knitting/sourcing the documents in this repo
+
+if (!"devtools" %in% installed.packages()) {
+  install.packages("devtools")
+}
+
+deps <- list(
+  "rlang"    = quote(install.packages("rlang")),
+  "jsvm"     = quote(devtools::install_github("janusvm/jsvm")),
+  "xaringan" = quote(devtools::install_github("yihui/xaringan")),
+  "emo"      = quote(devtools::install_github("hadley/emo"))
+)
+
+installed <- names(deps) %in% installed.packages()
+
+# Install missing packages
+for (i in seq_along(deps)) {
+  if (!installed[i]) eval(deps[[i]])
+}
